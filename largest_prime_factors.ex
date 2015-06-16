c235b9097d4fcb7a82533c1cc1b77dd770e329c1
exosyphon <exosyphon@yahoo.com>
2015-06-15 23:19:22 -0600

    More reasonable Problem 3

--- largest_prime_factors.ex
+++ largest_prime_factors.ex
@@ -1,13 +1,21 @@
 defmodule LargestPrimeFactors do
-  def call(seed) do
-   divisible_numbers = Enum.filter(2..seed-1, fn(x) -> rem(seed, x) == 0 and prime?(x) end) 
-   List.last(Enum.to_list(divisible_numbers))
-  end
-
   defp prime?(num) do
     Enum.all?(2..num-1, fn(x) -> rem(num, x) != 0 end)
   end
+
+  defp prime_factors(number, div \\ 2, factors \\ []) do
+    cond do
+      prime?(number) ->
+        [number | factors]
+      rem(number, div) == 0 ->
+        prime_factors(div(number, div), 2, [div | factors])
+      true ->
+        prime_factors(number, div + 1, factors)
+    end
+  end
+
+  def largest_prime(n), do: prime_factors(n) |> List.first
 end
 
-IO.puts "test #{LargestPrimeFactors.call(13195)}"
-IO.puts "real #{LargestPrimeFactors.call(600_851_475_143)}"
+IO.puts "test #{LargestPrimeFactors.largest_prime(13195)}"
+IO.puts "real #{LargestPrimeFactors.largest_prime(600_851_475_143)}"
